9acd17ba6842fa73a87743551afb3ab62f4c8ee1
Pavan Kumar VIsakoti <123760529+visakotipavankumar047@users.noreply.github.com>
2024-07-22 10:20:23 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
